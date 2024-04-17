34f07e787f797c3b52cf80a54b646e3cc3fb3c7a
Yadavalli sai kamesh <145605035+kalasaikamesh@users.noreply.github.com>
2024-04-17 07:46:00 +0530

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -1,1 +1,8 @@
+############№################################
 import sys
+import os
+import subprocess 
+from rich import print
+import csv
+import re
+##############################################

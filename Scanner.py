0999985f64ac9786f49b04b93644c5c8d9324705
Yadavalli sai kamesh <145605035+kalasaikamesh@users.noreply.github.com>
2024-04-17 07:59:32 +0530

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -5,4 +5,19 @@ import subprocess
 from rich import print
 import csv
 import re
+from time import sleep 
 ##############################################
+def start_mon(interface):
+  print(f"Changing to {interface}mon")
+  sleep(2)
+  os.system(f"airmon-ng start {interface}mon")
+  print(f"Changed to {interface}mon")
+
+def stop_mon(interface):
+  print(f"Changing to {interface}")
+  slee(2)
+  os.system(f"airmon-ng stop {interface}mon")
+  print(f"Changed to {interface}")
+
+def scan(interface):
+  pass

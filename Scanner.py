06a956ba13e8bea0d6efc05e5fd9b4722282f8ec
Yadavalli sai kamesh <145605035+kalasaikamesh@users.noreply.github.com>
2024-04-18 22:08:22 +0530

    Update Scanner.py
    Still Coding in progress

--- Scanner.py
+++ Scanner.py
@@ -19,6 +19,20 @@ def stop_mon(interface):
   os.system(f"airmon-ng stop {interface}mon")
   print(f"Changed to {interface}")
 
+def check_for_essid(essid, lst):
+    check_status = True
+
+
+    if len(lst) == 0:
+        return check_status
+
+
+    for item in lst:
+
+        if essid in item["ESSID"]:
+            check_status = False
+
+    return check_status
 
 def scan(interface):
   for file_name in os.listdir():
@@ -34,6 +48,28 @@ def scan(interface):
             print("Backup folder exists.")
         timestamp = datetime.now()
         shutil.move(file_name, directory + "/backup/" + str(timestamp) + "-" + file_name)
+        discover_access_points = subprocess.Popen(["sudo", "airodump-ng","-w" ,"file","--write-interval", "1","--output-format", "csv", hacknic + "mon"], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
 
+        try:
+            while True:
+                subprocess.call("clear", shell=True)
 
+                for file_name in os.listdir():
+                    fieldnames = ['BSSID', 'First_time_seen', 'Last_time_seen', 'channel', 'Speed', 'Privacy', 'Cipher', 'Authentication', 'Power', 'beacons', 'IV', 'LAN_IP', 'ID_length', 'ESSID', 'Key']
+
+                    if ".csv" in file_name:
+                        with open(file_name) as csv_h:
+                             csv_h.seek(0)
+                             csv_reader = csv.DictReader(csv_h, fieldnames=fieldnames)
+
+                             for row in csv_reader:
+                                 if row["BSSID"] == "BSSID":
+                                    pass
+                                 elif row["BSSID"] == "Station MAC":
+                                    break
+                                 elif check_for_essid(row["ESSID"], active_wireless_networks):
+                                    active_wireless_networks.append(row)
+                              
+                 for index, item in enumerate(active_wireless_networks):
+                       return item["BSSID"] , item["channel"]
       
